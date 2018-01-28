89d8db4524b4428817d93c2fab1deda570858c1b
Glitch (checktime) <none>
2018-01-28 08:52:59 +0000

    💐🏡 Updated with Glitch, added api call to get current time, timezone can be applied

--- server.js
+++ server.js
@@ -11,9 +11,38 @@ var app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.all('/api/*', (req, res, next) => {
+  console.log(JSON.stringify(req.headers));
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/current_time", (req, res) => {
+  
+  var timeZone = parseFloat(req.headers.timezone);
+  var time = new Date().getTime();
+  if(!isNaN(timeZone))
+  {
+    console.log(timeZone);
+    var date = new Date();
+    var localTime = date.getTime();
+    var localOffset = date.getTimezoneOffset() * 60000;  
+    time = localTime + localOffset + (3600000 * timeZone);    
+  }  
+  
+  var currentTime = {
+    unix: Math.floor(time / 1000),
+    date: new Date(time).toString(),
+  };
+  res.end(JSON.stringify(currentTime));
+});
+
+app.get('/api/whoami', (req, res) => {
+  res.end(JSON.stringify(whoami(req.headers)));
 });
 
 app.post("/time_input", function (request, response) {
@@ -33,7 +62,7 @@ app.post("/time_input", function (request, response) {
   else if (isNaN(timestamp) == false)
   {
       date = new Date(timestamp);
-      unixStamp = date.getTime()/1000;   
+      unixStamp = Math.floor(date.getTime()/1000);   
   }  
   
   var locale = "en-us";
@@ -49,3 +78,18 @@ app.post("/time_input", function (request, response) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+var whoami = (headers) => 
+{  
+  var agent = headers["user-agent"];
+  var fromIndex = agent.indexOf('(');
+  var toIndex = agent.indexOf(')');
+  
+  var me = {
+    ipaddress: headers["x-forwarded-for"].split(',')[0],
+    language: headers["accept-language"].split(';')[0].split(',')[0],
+    software: agent.substring(fromIndex + 1, toIndex),
+  }
+  
+  return me;
+};
